583a620e60917e7e70018e9ca55b9a0aaeaca047
AWizardNamedGandalf <AWizardNamedGandalf@proton.me>
2025-09-04 20:06:43 -0700

    Created initial text and comments

---
+++ CalculatorApp/IsThisACalculator.cpp
@@ -0,0 +1,23 @@
+//Yes this is the microsoft tutorial
+//Yes, it seems you are reading me notes for some reason
+
+#include <iostream>
+//oh let me tell you what this does, apparently #include brings in code from other files...
+//... in this case it is looking for iostream, whatever that does
+
+
+using namespace std;
+// no std is not a disease in this case, its shorthand for standard
+//I think we should not make fun of people with shorthands
+
+
+int main()
+//as in its mainly code
+{
+	cout << "Calculator Console Application" << endl << endl;
+	//I think cout means character output but idk who knows what << means
+	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
+	//This is in english. I wonder what coding in other languages is like
+	return 0;
+	//That 0 is actually a donut/doughnut, this should return me a doughnut if it works
+}
