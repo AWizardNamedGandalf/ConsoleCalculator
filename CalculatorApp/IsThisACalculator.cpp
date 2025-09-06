b8c2350d695e5ee1f55b5f9201b7edcee2b367d9
AWizardNamedGandalf <AWizardNamedGandalf@proton.me>
2025-09-06 11:29:24 -0700

    calculations implimented

--- CalculatorApp/IsThisACalculator.cpp
+++ CalculatorApp/IsThisACalculator.cpp
@@ -2,6 +2,7 @@
 //Yes, it seems you are reading me notes for some reason
 
 #include <iostream>
+#include "OoohClassy.h"
 //oh let me tell you what this does, apparently #include brings in code from other files...
 //... in this case it is looking for iostream, whatever that does
 
@@ -14,14 +15,21 @@ using namespace std;
 int main()
 //as in its mainly code
 {
+	double x = 0.0;
+	//double means doulbe the amount of point :)
+	double y = 0.0;
+	//ok its specifies the tpye of number like int or float, in this case double whatever that is
+	double result = 0.0;
+	//hmmmmmm
+	char oper = '+';
+	//get to the char oper!!
+
+
 	cout << "Calculator Console Application" << endl << endl;
 	//I think cout means character output but idk who knows what << means
 	//Oh i actually read stuff and << tells com-pile-er to send whatever is to the right of it to the std!!  
 	cout << "Please enter the operation to perform. Format: a+b | a-b | a*b | a/b" << endl;
 	//This is in english. I wonder what coding in other languages is like
-<<<<<<< Updated upstream
-=======
-
 
 
 	OoohClassy c;
@@ -32,7 +40,6 @@ int main()
 		cout << "Result " << "of " << x << oper << y << " is: " << result << endl;
 	}
 
->>>>>>> Stashed changes
 	return 0;
 	//That 0 is actually a donut/doughnut, this should return me a doughnut if it works
 }
